bfa939780c2adec4e9d026ec72e43e05d8e4032e
rafa30052003 <rafalucenavalle9@gmail.com>
2023-04-13 17:05:23 +0200

    modoOscuro

--- src/main/java/iesfranciscodelosrios/LoginFX/App.java
+++ src/main/java/iesfranciscodelosrios/LoginFX/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("admin"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -34,5 +34,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
+    
 }
